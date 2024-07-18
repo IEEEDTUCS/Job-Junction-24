b8ef51c17075489cc5de8daf2bb2a9b6d08296c9
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-18 14:23:42 +0530

    JobList component

--- frontend/src/components/JobPortal.Contents/Filters.jsx
+++ frontend/src/components/JobPortal.Contents/Filters.jsx
@@ -2,18 +2,16 @@ import React from 'react'
 
 const Filters = () => {
   return (
-    <div className='bg-white rounded-md'>
-      <div className="flex justify-between items-center text-xs p-2">
+      <div className="flex justify-between items-center text-xs p-2 bg-white rounded-md">
         <div className='flex items-center ml-[0.45rem]'>
-          <button className='bg-green-600 text-white rounded-md p-2'>Best Matches</button>
-          <div className='ml-[0.94rem] text-gray-500'>Most Recent</div>
+          <button className='bg-green-600 text-white rounded-md py-2 px-4'>Best Matches</button>
+          <div className='text-gray-500 py-2 px-4'>Most Recent</div>
         </div>
         <div className='flex text-gray-500 mr-[0.45rem]'>
           <div>Filters</div>
           <img alt='menu icon' src='src/assets/menu.svg' className='ml-[0.31rem]' />
         </div>
       </div>
-    </div>
   )
 }
 
--- frontend/src/components/JobPortal.Contents/JobList.jsx
+++ frontend/src/components/JobPortal.Contents/JobList.jsx
@@ -4,25 +4,29 @@ import React from 'react'
 // List of job posts
 const JobList = () => {
   return (
-    <div style={{ border: '1px solid red' }} className='p-1 bg-white text-green-600 rounded-md'>
-      <div className='flex'>
-        <img src='src/assets/company_logo.svg' />
-        <div>
-          <span className='font-bold'>Software Developer</span>
-          <span>{'3 days'} ago</span>
-          <p className='font-semibold'>We are looking for figma designers who can help desiging the entire mobile application ...</p>
+    <div className='p-5 bg-white text-green-600 rounded-md border-2 border-green-600'>
+      <div className='flex items-start pb-4'>
+        <img src='src/assets/company_logo.svg' className='w-32'/>
+        <div className='px-4'>
+          <div className='flex justify-between'>
+            <span className='font-bold text-xl'>Software Developer</span>
+            <span className='font-normal'>{'3 days'} ago</span>
+          </div>
+          <p className='font-medium'>We are looking for figma designers who can help desiging the entire mobile application...</p>
         </div>
-        <img src='src/assets/bookmark.svg' />
+        <img src='src/assets/bookmark.svg' className='w-12'/>
       </div>
-      <div className='flex gap-1'>
-        <span className='text-[#656565] bg-[#EFEFEF] rounded-md p-1'>Ui Designer</span>
-        <span className='text-[#656565] bg-[#EFEFEF] rounded-md p-1'>Ui Designer</span>
-        <span className='text-[#656565] bg-[#EFEFEF] rounded-md p-1'>Ui Designer</span>
+      <div className='flex gap-4'>
+        <span className='text-[#656565] bg-[#EFEFEF] rounded-md py-2 px-3 text-xs'>{'Ui Designer'}</span>
+        <span className='text-[#656565] bg-[#EFEFEF] rounded-md py-2 px-3 text-xs'>{'Figma'}</span>
+        <span className='text-[#656565] bg-[#EFEFEF] rounded-md py-2 px-3 text-xs'>{'Landing Page'}</span>
       </div>
-      <div className='flex justify-between'>
-        <span><span className='font-medium'>Deadline:</span> {'10th Feb 2025'}</span>
-        <span><span className='font-medium'>Applied:</span> {'1000'}</span>
-        <img src='src/assets/platform.svg' />
+      <div className='flex justify-between items-end'>
+        <div className='flex gap-8'>
+          <span><span className='font-medium'>Deadline:</span> {'10th Feb 2025'}</span>
+          <span><span className='font-medium'>Applied:</span> {'1000'}</span>
+        </div>
+        <img src='src/assets/platform.svg' className='w-12'/>
       </div>
     </div>
   )
